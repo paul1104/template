741caafedbb0d39aa0c83b223920145449c21005
Faraaz Ahmad <33687170+zFz0000@users.noreply.github.com>
2017-12-20 10:11:37 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LineTiny_Polosan
+Ini script LineTinySDK versi medantechno.com
+Ini script polosan dengan 1 contoh keyword
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+/*
+copyright @ medantechno.com
+2017
+
+*/
+
+require_once('./line_class.php');
+
+$channelAccessToken = ''; //sesuaikan 
+$channelSecret = '';//sesuaikan
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+$userId 	= $client->parseEvents()[0]['source']['userId'];
+$replyToken = $client->parseEvents()[0]['replyToken'];
+$timestamp	= $client->parseEvents()[0]['timestamp'];
+$message 	= $client->parseEvents()[0]['message'];
+$messageid 	= $client->parseEvents()[0]['message']['id'];
+$profil = $client->profil($userId);
+$pesan_datang = $message['text'];
+
+//pesan bergambar
+if($message['type']=='text')
+{
+	if($pesan_datang=='Halo')
+	{
+		
+		
+		$balas = array(
+							'replyToken' => $replyToken,														
+							'messages' => array(
+								array(
+										'type' => 'text',					
+										'text' => 'Halo'
+									)
+							)
+						);
+				
+	}
+ 
+$result =  json_encode($balas);
+//$result = ob_get_clean();
+
+file_put_contents('./balasan.json',$result);
+
+
+$client->replyMessage($balas);
