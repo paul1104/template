c11fb797b6f7602642540c8bce6d6539a60286aa
syahraqagr <32156061+syahraqagr@users.noreply.github.com>
2018-06-09 09:34:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,75 @@ $timestamp	= $client->parseEvents()[0]['timestamp'];
 $message 	= $client->parseEvents()[0]['message'];
 $messageid 	= $client->parseEvents()[0]['message']['id'];
 $profil = $client->profil($userId);
-$pesan_datang = $message['text'];
+
+$pesan_datang = explode(" ", $message['text']);
+
+$command = $pesan_datang[0];
+$options = $pesan_datang[1];
+if (count($pesan_datang) > 2) {
+    for ($i = 2; $i < count($pesan_datang); $i++) {
+        $options .= '+';
+        $options .= $pesan_datang[$i];
+    }
+}
+
+#-------------------------[Function]-------------------------#
+function shalat($keyword) {
+    $uri = "https://time.siswadi.com/pray/" . $keyword;
+    $response = Unirest\Request::get("$uri");
+    $json = json_decode($response->raw_body, true);
+    $result = "Jadwal Shalat Sekitar ";
+	$result .= $json['location']['address'];
+	$result .= "\nTanggal : ";
+	$result .= $json['time']['date'];
+	$result .= "\n\nShubuh : ";
+	$result .= $json['data']['Fajr'];
+	$result .= "\nDzuhur : ";
+	$result .= $json['data']['Dhuhr'];
+	$result .= "\nAshar : ";
+	$result .= $json['data']['Asr'];
+	$result .= "\nMaghrib : ";
+	$result .= $json['data']['Maghrib'];
+	$result .= "\nIsya : ";
+	$result .= $json['data']['Isha'];
+    return $result;
+}
+#-------------------------[Function]-------------------------#
+# require_once('./src/function/search-1.php');
+# require_once('./src/function/download.php');
+# require_once('./src/function/random.php');
+# require_once('./src/function/search-2.php');
+# require_once('./src/function/hard.php');
+
+//show menu, saat join dan command /menu
+if ($type == 'join' || $command == '/menu') {
+    $text = "Assalamualaikum Kakak, aku adalah bot jadwal shalat, silahkan ketik\n\n/shalat <nama tempat>\n\nnanti aku bakalan kasih tahu jam berapa waktunya shalat ^_^";
+    $balas = array(
+        'replyToken' => $replyToken,
+        'messages' => array(
+            array(
+                'type' => 'text',
+                'text' => $text
+            )
+        )
+    );
+}
+
+//pesan bergambar
+if($message['type']=='text') {
+	    if ($command == '/shalat') {
+        $result = shalat($options);
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => $result
+                )
+            )
+        );
+    }
+}
 
 //pesan bergambar
 
